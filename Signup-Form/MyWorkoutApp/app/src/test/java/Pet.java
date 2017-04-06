109ce69be0268040c4c1b2ba9e14e3182f756b26
jrod387 <jasonrod387@gmail.com>
2017-04-06 17:53:24 -0400

    added workout app

---
+++ Signup-Form/MyWorkoutApp/app/src/test/java/Pet.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Owner on 4/3/2017.
+ */
+
+public class Pet {
+    private int mNumberOfPets;
+    private String mColour;
+    private int mNumberOfFeet;
+    private String mSpecies;
+    private String mOwner;
+
+public Pet(int numberOfPets, String colour, int numberOfFeet, String species, String owner){
+    mNumberOfPets = numberOfPets;
+    mColour = colour;
+    mNumberOfFeet = numberOfFeet;
+    mSpecies = species;
+    mOwner = owner;
+}
+
+
+
+
+}
