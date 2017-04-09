c7ea14c57396680f47fa4f8d612591d99c9033e7
John Ruddock <john.ruddock@hotmail.com>
2017-04-08 20:39:58 -0400

    April 8/17
    Did quiz on Toast, created user class, created UI for Signup form and
    gave all views new ID's

---
+++ Signup-Form/MyApplication/app/src/main/java/com/example/john/myapplication/User.java
@@ -0,0 +1,8 @@
+package com.example.john.myapplication;
+
+/**
+ * Created by John on 4/8/2017.
+ */
+
+public class User {
+}
---
+++ Signup-Form/SignupFormV1/app/src/main/java/com/example/john/signupformv1/User.java
@@ -0,0 +1,33 @@
+package com.example.john.signupformv1;
+
+/**
+ * Created by John on 4/8/2017.
+ */
+
+public class User {
+    private String email;
+    private String password;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+
+    }
+}
