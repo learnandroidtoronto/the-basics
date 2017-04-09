854750fc4baddefff99d693095f3401132d6259d
danieljf06 <danielfaria619@hotmail.com>
2017-04-08 20:43:47 -0400

    april 8,2016
    we created a user class, we did a quiz on toast, and finally we created
    the UI for a sign up form and gave all the views their ID's. respects.

---
+++ Signup-Form/Sign-upFormV1/app/src/main/java/com/example/daniel/sign_upformv1/User.java
@@ -0,0 +1,31 @@
+package com.example.daniel.sign_upformv1;
+
+/**
+ * Created by daniel on 2017-04-08.
+ */
+
+public class User {
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
