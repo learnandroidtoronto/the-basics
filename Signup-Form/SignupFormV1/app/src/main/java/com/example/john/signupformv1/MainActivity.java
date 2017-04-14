3736e8df432a4a7ed1c51000d300661a8b0ca2e6
John Ruddock <john.ruddock@hotmail.com>
2017-04-14 19:34:34 -0400

    This Week's Practice
    linked my Button and editText views to the Main Activity class

--- Signup-Form/SignupFormV1/app/src/main/java/com/example/john/signupformv1/MainActivity.java
+++ Signup-Form/SignupFormV1/app/src/main/java/com/example/john/signupformv1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.john.signupformv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText emailEditText = (EditText)findViewById(R.id.email_editText);
+        EditText passwordEditText = (EditText)findViewById(R.id.password_editText);
+
+        Button createButton = (Button)findViewById(R.id.create_button);
+        Button loginButton = (Button)findViewById(R.id.login_button);
     }
 }
