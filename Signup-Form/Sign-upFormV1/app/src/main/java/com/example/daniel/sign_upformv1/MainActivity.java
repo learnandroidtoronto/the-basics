823f4c36ef29d623b7e57fea4945d48f78e2e170
danieljf06 <danielfaria619@hotmail.com>
2017-04-14 19:07:26 -0400

    April 14, 2017
    hooked up the view to MainActivity.java

--- Signup-Form/Sign-upFormV1/app/src/main/java/com/example/daniel/sign_upformv1/MainActivity.java
+++ Signup-Form/Sign-upFormV1/app/src/main/java/com/example/daniel/sign_upformv1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.daniel.sign_upformv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    TextView EmailEditTextTextview = (TextView) findViewById(R.id.EmailEditText);
+
+    TextView PasswordEditTextTextview = (TextView) findViewById(R.id.PasswordEditText);
+
+    Button CreateButtonButton = (Button) findViewById(R.id.CreateButton);
+
+    Button LoginButtonButton = (Button) findViewById(R.id.LoginButton);
 }
