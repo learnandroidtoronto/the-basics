76743cce3e015fe010f18cc5f0f1b2ee53babd54
jrod387 <jasonrod387@gmail.com>
2017-04-14 21:57:02 -0400

    This weeks practice
    i have connected the view id`s from activity_main.xml to
    Main.Activity.java. I created a method to locate these buttons by the
    tittle i have given them

--- Signup-Form/signupformv12/app/src/main/java/com/example/owner/signupformv1/MainActivity.java
+++ Signup-Form/signupformv12/app/src/main/java/com/example/owner/signupformv1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.owner.signupformv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    Button createButton = (Button)findViewById(R.id.createButton);
+    Button loginButton = (Button)findViewById(R.id.loginButton);
+    EditText password_Text = (EditText)findViewById(R.id.password_Text);
+    EditText login_Text = (EditText)findViewById(R.id.login_Text);
 }
