e43de580a4286d07ef50dc7f1f8a844f856e25ad
Dannydsinipi <danieldacosta@Daniels-MacBook-Air.local>
2017-04-15 20:16:36 -0400

    This week's practice
    hook up  views with the ids

--- Signup-Form/SignupformV1/app/src/main/java/com/example/danieldacosta/signupformv1/MainActivity.java
+++ Signup-Form/SignupformV1/app/src/main/java/com/example/danieldacosta/signupformv1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.danieldacosta.signupformv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText email_edittext = (EditText) findViewById(R.id.email_edittext)
+        EditText password_edittext = (EditText) findViewById(R.id.password_edittext)
+        Button CreateButtonButton = (Button) findViewById(R.id.Create_account)
+        Button CreateButtonButton = (Button) findViewById(R.id.login_button)
+
+
 
 
     }
