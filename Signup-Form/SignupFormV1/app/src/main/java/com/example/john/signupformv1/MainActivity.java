21108d76a252d9ce39510e8dac8a0e2d34bff77b
John Ruddock <john.ruddock@hotmail.com>
2017-04-20 20:44:23 -0400

    April 20, 2017
    Added onClick attribute to xml, made Edit Text variables member
    variables, made a createUser method in MainActivity class, we made
    profileUser of type User and we assigned it to the constructor. We gave
    it the parameters of emailEditText and passwordEditText which converted
    to Strings. Then created a toast message to show the user's input for
    email and password. Also changed the app name.

--- Signup-Form/SignupFormV1/app/src/main/java/com/example/john/signupformv1/MainActivity.java
+++ Signup-Form/SignupFormV1/app/src/main/java/com/example/john/signupformv1/MainActivity.java
@@ -2,20 +2,32 @@ package com.example.john.signupformv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private EditText passwordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText emailEditText = (EditText)findViewById(R.id.email_editText);
-        EditText passwordEditText = (EditText)findViewById(R.id.password_editText);
+        emailEditText = (EditText)findViewById(R.id.email_editText);
+        passwordEditText = (EditText)findViewById(R.id.password_editText);
 
         Button createButton = (Button)findViewById(R.id.create_button);
         Button loginButton = (Button)findViewById(R.id.login_button);
+
+
+    }
+
+    public void createUser(View v){
+        User profileUser = new User(emailEditText.getText().toString(), passwordEditText.getText().toString());
+        Toast.makeText(this, "Email: " + profileUser.getEmail() + "\nPassword: " + profileUser.getPassword(), Toast.LENGTH_LONG).show();
     }
 }
--- Signup-Form/SignupFormV1/app/src/main/java/com/example/john/signupformv1/User.java
+++ Signup-Form/SignupFormV1/app/src/main/java/com/example/john/signupformv1/User.java
@@ -22,6 +22,7 @@ public class User {
     }
 
     public String getPassword() {
+
         return password;
     }
 
