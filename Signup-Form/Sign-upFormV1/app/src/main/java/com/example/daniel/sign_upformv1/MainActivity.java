c88d5c494878b30171eb894b059b5219b6bc0024
danieljf06 <danielfaria619@hotmail.com>
2017-04-20 20:49:13 -0400

    April 20, 2017
    -added android on click attribute to xml
    - we made our editText variables to member variables
    -we made profileUser of type User and we assigned it to the constructor,
    we gave it the perameters of email edit text and password edit text. We
    than called methods on those perameters to convert the parameters to
    strings. We followed with a toast message, it displays a short message.
    we got it to show by putting in the "show" method after the parameters.

--- Signup-Form/Sign-upFormV1/app/src/main/java/com/example/daniel/sign_upformv1/MainActivity.java
+++ Signup-Form/Sign-upFormV1/app/src/main/java/com/example/daniel/sign_upformv1/MainActivity.java
@@ -2,23 +2,35 @@ package com.example.daniel.sign_upformv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailEditText = (EditText) findViewById(R.id.EmailEditText);
+
+        passwordEditText = (EditText) findViewById(R.id.PasswordEditText);
+
+        Button createButton = (Button) findViewById(R.id.CreateButton);
+
+        Button loginButton = (Button) findViewById(R.id.LoginButton);
     }
 
-    EditText EmailEditTextEditText = (EditText) findViewById(R.id.EmailEditText);
+    public void createUser( View v ){
+        User profileUser = new User(emailEditText.getText().toString(),passwordEditText.getText().toString());
 
-    EditText PasswordEditTextEditText = (EditText) findViewById(R.id.PasswordEditText);
+        Toast.makeText(this,"email: " + profileUser.getEmail()+ "\n" + "password: " + profileUser.getPassword(), Toast.LENGTH_LONG).show();
+    }
 
-    Button CreateButtonButton = (Button) findViewById(R.id.CreateButton);
 
-    Button LoginButtonButton = (Button) findViewById(R.id.LoginButton);
 }
