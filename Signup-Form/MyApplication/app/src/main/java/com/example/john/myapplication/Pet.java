91ba344fb798baa77f742de4cd07a753f6213fb2
John Ruddock <john.ruddock@hotmail.com>
2017-04-22 19:17:58 -0400

    April 22/17
    Removed myapplication folder from signup form

--- Signup-Form/MyApplication/app/src/main/java/com/example/john/myapplication/Pet.java
+++
@@ -1,9 +0,0 @@
-package com.example.john.myapplication;
-
-/**
- * Created by John on 4/3/2017.
- */
-
-public class Pet {
-    private String mOwnerJohn;
-}
--- Signup-Form/MyApplication/app/src/main/java/com/example/john/myapplication/User.java
+++
@@ -1,8 +0,0 @@
-package com.example.john.myapplication;
-
-/**
- * Created by John on 4/8/2017.
- */
-
-public class User {
-}
