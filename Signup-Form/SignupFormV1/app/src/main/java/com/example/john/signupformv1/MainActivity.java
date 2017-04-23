5c5dbe86e2eb7a9ef7709eb6479c79b4dd679101
John Ruddock <john.ruddock@hotmail.com>
2017-04-22 22:18:41 -0400

    April 22, 2017
    made our OnClick listener in java instead of XML. created an if else
    statement which applies to the creation of a user. created a listener
    object( overode method. Stored new users in member variable
    userDatabase. Compared strings in the if statement with "equals" text
    rather than "==".

--- Signup-Form/SignupFormV1/app/src/main/java/com/example/john/signupformv1/MainActivity.java
+++ Signup-Form/SignupFormV1/app/src/main/java/com/example/john/signupformv1/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private EditText emailEditText;
     private EditText passwordEditText;
+    private User userDatabase = new User("", "");
+
+    //___ ArrayList<User> userList;
 
 
     @Override
@@ -17,17 +20,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        };
+
         emailEditText = (EditText)findViewById(R.id.email_editText);
         passwordEditText = (EditText)findViewById(R.id.password_editText);
 
         Button createButton = (Button)findViewById(R.id.create_button);
         Button loginButton = (Button)findViewById(R.id.login_button);
 
+        createButton.setOnClickListener(listener);
+
+
+
 
     }
-
-    public void createUser(View v){
+    // == != > < >= <=
+    public void createUser(){
         User profileUser = new User(emailEditText.getText().toString(), passwordEditText.getText().toString());
-        Toast.makeText(this, "Email: " + profileUser.getEmail() + "\nPassword: " + profileUser.getPassword(), Toast.LENGTH_LONG).show();
+        if (!profileUser.getEmail().equals(userDatabase.getEmail())) {
+            userDatabase = profileUser;
+            Toast.makeText(this, "Email: " + profileUser.getEmail() + "\nPassword: " + profileUser.getPassword(), Toast.LENGTH_LONG).show();
+
+        } else {
+            Toast.makeText(this, "This account already exists, go away", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
