1b8ea6907088de78030a802ba3ed295f3e387f31
jrod387 <jasonrod387@gmail.com>
2017-04-22 22:22:08 -0400

    April 22
    We created an object called user1 which would give us the username and
    the password, which would then be stored into the variable we created
    known as userStorage. We also created a toast message that would display
    the username and password. We created a toast message for each scenario
    ; if the user had already been selected or if a new user  was entered.
    We then created a listener object so that we would no longer need the
    onClick method in the xml. Instead we created the the Listener object to
    use the onClick method to run the program.

--- Signup-Form/signupformv12/app/src/main/java/com/example/owner/signupformv1/MainActivity.java
+++ Signup-Form/signupformv12/app/src/main/java/com/example/owner/signupformv1/MainActivity.java
@@ -2,18 +2,51 @@ package com.example.owner.signupformv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText password_Text;
+    private EditText login_Text;
+    private User userStorage = new User( " " , " ");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button createButton = (Button) findViewById(R.id.createButton);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        password_Text = (EditText) findViewById(R.id.password_Text);
+        login_Text = (EditText) findViewById(R.id.login_Text);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        };
+        createButton.setOnClickListener(listener);
+
     }
-    Button createButton = (Button)findViewById(R.id.createButton);
-    Button loginButton = (Button)findViewById(R.id.loginButton);
-    EditText password_Text = (EditText)findViewById(R.id.password_Text);
-    EditText login_Text = (EditText)findViewById(R.id.login_Text);
+
+
+    public void createUser() {
+        User user1 = new User(login_Text.getText().toString(), password_Text.getText().toString());
+
+        if (user1.getEmail().equals(userStorage.getEmail()))
+        {
+            Toast.makeText(this, "Already In Use", Toast.LENGTH_LONG).show();
+
+        }
+        else {
+            userStorage=user1;
+            Toast.makeText(this, "UserName: " + user1.getEmail() + "\nPassword: " + user1.getPassword(), Toast.LENGTH_LONG).show();
+        }
+
+
+
+    }
 }
